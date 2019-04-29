9a24acbc22cc19f9f0387daba47fa15a49d9d478
Darren Shankland <ds@4pq.co.uk>
2019-04-29 10:41:51 +0100

    refactor pluralFixer and cut down on attributes in HTML, add parameter to deleteRecipeButtonClick

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-const deleteRecipeButtonClick = function () {
+const deleteRecipeButtonClick = function (someNode) {
   // tried passing the item as a parameter but no joy
-  const myNode = document.querySelector("#recipe-list");
-  while (myNode.firstChild) {
-    myNode.removeChild(myNode.firstChild);
+  // parameter work when calling function wrapped in an arrow function
+  // const myNode = document.querySelector("#recipe-list");
+  while (someNode.firstChild) {
+    someNode.removeChild(someNode.firstChild);
   };
 };
 
@@ -29,15 +30,24 @@ const formSubmit = function (evt) {
   form.reset();
 };
 
+
+const jsUcfirst = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 const pluralFixer = function () {
   const plurals = document.querySelectorAll('.plurals');
   plurals.forEach(function(plural) {
     if (quantity.value == 1) {
+      // this could be improved if I use a function to upper case the first char
+      // also means I can dispense with the data-single and data-other vals in the labels in HTML
       plural.value = plural.getAttribute('data-single');
-      plural.nextElementSibling.innerText = plural.nextElementSibling.getAttribute('data-single');
+      // plural.nextElementSibling.innerText = plural.nextElementSibling.getAttribute('data-single');
+      plural.nextElementSibling.innerText = jsUcfirst(plural.value);
     } else {
       plural.value = plural.getAttribute('data-other');
-      plural.nextElementSibling.innerText = plural.nextElementSibling.getAttribute('data-other')
+      // plural.nextElementSibling.innerText = plural.nextElementSibling.getAttribute('data-other')
+      plural.nextElementSibling.innerText = jsUcfirst(plural.value);
     };
   });
 };
@@ -79,7 +89,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const buttonLocation = document.body;
   const buttonChildrenToDelete = "#recipe-list";
   const newButton = createNewButton(buttonHTML, buttonClass, buttonLocation);
-  newButton.addEventListener("click", deleteRecipeButtonClick);
+  // newButton.addEventListener("click", deleteRecipeButtonClick);
+  const myNode = document.querySelector(buttonChildrenToDelete);
+  // passing this as a parameter to the deleteRecipeButtonClick function
+  // needs to be wrapped in an arrow functions as the function is not defined
+  // when the page loads
+  newButton.addEventListener("click", () => {
+    deleteRecipeButtonClick(myNode)
+  });
 
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', formSubmit);
